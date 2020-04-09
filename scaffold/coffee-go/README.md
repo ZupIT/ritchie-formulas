052b4d3d0ab21d9b6a91acd182cef8fd39ea1d9a
Sandokan <sandokandias@gmail.com>
2020-04-09 09:53:30 -0300

    test formula, add readme

---
+++ scaffold/coffee-go/README.md
@@ -0,0 +1,9 @@
+## coffee-go
+
+### command
+```bash
+$ rit scaffold generate coffee
+```
+
+### description
+This formula receive 3 inputs (customer name, coffee type, delivery, no delay) and builds a coffee.
--- scaffold/coffee-go/src/pkg/coffee/coffee.go
+++ scaffold/coffee-go/src/pkg/coffee/coffee.go
@@ -1,28 +1,47 @@
 package coffee
 
 import (
+	"errors"
 	"log"
 	"time"
+)
+
+var   (
+	ErrNameIsRequired = errors.New("the customer name is required")
 )
 
 type Inputs struct {
 	Name string
 	CoffeeType string
 	Delivery bool
+	NoDelay bool
 }
 
-func GiveMeSomeCoffee(inputs Inputs) {
-	log.Printf("Preparing your coffee %v .....\n", inputs.Name)
-	time.Sleep(time.Second * 1)
+
+
+func GiveMeSomeCoffee(i Inputs) error {
+	if i.Name == "" {
+		return ErrNameIsRequired
+	}
+
+	log.Printf("Preparing your coffee %v .....\n", i.Name)
 	log.Println("......")
-	time.Sleep(time.Second * 1)
+	i.delay()
 	log.Println("......")
-	time.Sleep(time.Second * 1)
+	i.delay()
 	log.Println("......")
-	time.Sleep(time.Second * 1)
-	if inputs.Delivery {
-		log.Printf("Your %v coffee is ready, enjoy your trip\n", inputs.CoffeeType)
+	i.delay()
+	if i.Delivery {
+		log.Printf("Your %v coffee is ready, enjoy your trip\n", i.CoffeeType)
 	} else {
-		log.Printf("Your %v coffee is ready, have a seat and enjoy your drink\n", inputs.CoffeeType)
+		log.Printf("Your %v coffee is ready, have a seat and enjoy your drink\n", i.CoffeeType)
+	}
+
+	return nil
+}
+
+func (i Inputs) delay() {
+	if !i.NoDelay {
+		time.Sleep(time.Second * 1)
 	}
 }
