3f5478c923eebdc3bf5e1ac63c6097263ac632a8
Marcos Guimarães <marcosgmgm@gmail.com>
2020-04-28 14:49:57 -0300

    shell coffee

--- scaffold/coffee-shell/src/main.sh
+++ scaffold/coffee-shell/src/main.sh
@@ -3,4 +3,4 @@
 . ./coffee/coffee.sh --source-only
 #In sh for receive inputs of CLI use: $NAME, $COFFEE_TYPE and $DELIVERY for this exemple
 
-  run $NAME $COFFEE_TYPE $DELIVERY
+run $NAME $COFFEE_TYPE $DELIVERY
