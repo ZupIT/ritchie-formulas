f9f8634340f77bb2f7458fa16e566505f06bd0a6
Sandokan <sandokandias@gmail.com>
2020-06-08 21:29:17 -0300

    draft formula terraform aws vpc

---
+++ terraform/aws/vpc/src/go.mod
@@ -0,0 +1,8 @@
+module vpc
+
+go 1.14
+
+require (
+	github.com/fatih/color v1.9.0
+	github.com/google/uuid v1.1.1 // indirect
+)
---
+++ terraform/aws/vpc/src/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"os"
+	"vpc/pkg/vpc"
+)
+
+func main() {
+	vpc.Run(loadInputs())
+}
+
+func loadInputs() vpc.Inputs {
+	region := os.Getenv("REGION")
+	vpcName := os.Getenv("VPC_NAME")
+	vpcCIDR := os.Getenv("VPC_CIDR")
+	vpcAZS := os.Getenv("VPC_AZS")
+	customerName := os.Getenv("CUSTOMER_NAME")
+
+	return vpc.Inputs{
+		Region:       region,
+		VPCName:      vpcName,
+		VPCCIDR:      vpcCIDR,
+		VPCAZS:       vpcAZS,
+		CustomerName: customerName,
+	}
+}
