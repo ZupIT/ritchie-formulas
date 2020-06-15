8f5c627353071d943d9d06297002c3420fa5f621
Ernélio <erneliojunior@gmail.com>
2020-06-15 12:09:37 -0300

    fix configmap

--- kubernetes/clean/helm-configmap/src/helm-configmap/helm-configmap.sh
+++ kubernetes/clean/helm-configmap/src/helm-configmap/helm-configmap.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 run() {
-  export CONFIGMAP_LIST=$(kubectl -n kube-system get configmap | awk '{print $1}'|cut -f1 -d.|sort|uniq); for i in $CONFIGMAP_LIST;
+  CONFIGMAP_LIST=$(kubectl -n kube-system get configmap |awk '{print $1}'|cut -f1 -d.|sort|uniq|grep -v 'NAME');
+  for i in $CONFIGMAP_LIST;
   do echo $i;
   for j in `kubectl -n kube-system get configmap | grep $i| awk '{print $1}'|cut -d. -f2|cut -dv -f2| sort -n | head -n -2`; do echo $i.v$j; kubectl -n kube-system delete configmap $i.v$j ; done; done
 }
