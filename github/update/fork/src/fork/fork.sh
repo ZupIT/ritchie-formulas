8fed3e5956f9a26d96f6fa46f33e2dedacc8e37b
Fabiano Chiareto Fernandes <fabiano.fernandes@zup.com.br>
2020-06-30 18:19:26 -0300

    Formula Update Fork

---
+++ github/update/fork/src/fork/fork.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+run() {
+  echo "Updating repo local: $CURRENT_PWD"
+  cd $CURRENT_PWD
+  status=$(git status | grep "nothing to commit")
+  if [ "$status" != "nothing to commit, working tree clean" ] ;then
+    echo -e "✘️ \e[91mError: \e[0mis Commit or discard changes in working directory";exit 1;
+    exit 1;
+  fi
+  if $SETUPSTREAM; then
+    read -p "Type original repo: (https://github.com/ORIGINAL_OWNER/ORIGINAL_REPOSITORY.git) " originalrepo
+    git remote add upstream $originalrepo
+  fi
+  git checkout master
+  git fetch upstream
+  git rebase upstream/master master
+  if $PUSH; then
+    echo "Push"
+    git push
+  fi
+}
