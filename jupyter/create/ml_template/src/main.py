99853c8ecf3110e662644619741ae9e7b43617f6
Henrique Moraes <henrique.moraes@zup.com.br>
2020-07-09 17:54:43 -0300

    main correction

--- jupyter/create/ml_template/src/main.py
+++ jupyter/create/ml_template/src/main.py
@@ -6,4 +6,4 @@ from ml_template import ml_template
 datapoints_amount = os.environ.get('DATAPOINTS_AMOUNT')
 category = os.environ.get('CATEGORY')
 labeled_data = os.environ.get('LABELED_DATA')
-template.Run(datapoints_amount, category, labeled_data)
+ml_template.Run(datapoints_amount, category, labeled_data)
