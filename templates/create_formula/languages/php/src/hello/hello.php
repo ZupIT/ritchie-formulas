a4199384e5378c0fde593eb9c2054f40bd6d34a6
viniciusousazup <vinicius.sousa@zup.com.br>
2020-07-14 16:31:49 -0300

    add php

---
+++ templates/create_formula/languages/php/src/hello/hello.php
@@ -0,0 +1,8 @@
+<?php
+	function Run($input1, $input2, $input3) {
+		echo "Hello World! \n";
+		echo "You receive $input1 in text. \n";
+		echo "You receive $input2 in list. \n";
+		echo "You receive $input3 in boolean. \n";
+	}
+?>
