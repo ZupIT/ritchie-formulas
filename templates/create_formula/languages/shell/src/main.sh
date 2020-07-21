160f6f94d6aaa3c273b68583bf2d2e36943ff94a
Daniel Barra <dmbarra@gmail.com>
2020-07-21 13:27:27 -0300

    Fix shell script template

--- templates/create_formula/languages/shell/src/main.sh
+++ templates/create_formula/languages/shell/src/main.sh
@@ -1,4 +1,5 @@
-#!/bin/sh
+#!/bin/bash
+
 # shellcheck source=/dev/null
 . "$(dirname "$0")"/hello/hello.sh --source-only
 #In sh for receive inputs of CLI use: $SAMPLE_TEXT, $SAMPLE_LIST and $SAMPLE_BOOL for this example
