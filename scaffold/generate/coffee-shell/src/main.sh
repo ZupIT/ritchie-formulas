9c879a9fcf6b8c8c9b10c24853baddcbd4287ed6
viniciusousazup <vinicius.sousa@zup.com.br>
2020-07-24 16:26:13 -0300

    add build.bat add Dockerfile add metadata.json add set_umask.sh change makefile to use docker

--- scaffold/generate/coffee-shell/src/main.sh
+++ scaffold/generate/coffee-shell/src/main.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # shellcheck source=/dev/null
-. "$PWD"/coffee/coffee.sh --source-only
+. "$(dirname "$0")"/coffee/coffee.sh --source-only
 #In sh for receive inputs of CLI use: $NAME, $COFFEE_TYPE and $DELIVERY for this exemple
 
 run "$NAME" "$COFFEE_TYPE" "$DELIVERY"
