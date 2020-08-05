65bb5c8100939b53cbfc6088b3814e7fba160cb0
Fabiano Chiareto Fernandes <fabiano.fernandes@zup.com.br>
2020-08-05 20:43:43 -0300

    Fix Shell Syntax

--- github/update/fork/src/fork/fork.sh
+++ github/update/fork/src/fork/fork.sh
@@ -2,57 +2,53 @@
 
 run() {
   checkCommand git
-  echo -e "\\e[0;32m✔ \\e[1;30mUpdating repo local: "$CURRENT_PWD"\\e[0m";
-  echo -e "\\e[0;32m✔ \\e[1;30mUpdating branch: "$BRANCH"\\e[0m";
+  echo -e "\\e[0;32m✔ \\e[1;30mUpdating repo local: $CURRENT_PWD\\e[0m";
+  echo -e "\\e[0;32m✔ \\e[1;30mUpdating branch: $BRANCH\\e[0m";
   cd "$CURRENT_PWD" || exit
-  status=$(git status | grep "nothing to commit\|nada a submeter")
+  status=$(git status | grep "nothing to commit\\|nada a submeter")
   if [ "$status" != "nothing to commit, working tree clean" ] && [ "$status" != "nada a submeter, diretório de trabalho vazio" ] ;then
     echo -e "✘️ \\e[91mError:\\e[0m Commit or discard changes in working directory";
     exit 1;
   fi
 
   if $SETUPSTREAM; then
-    read -p "Type original repo: (https://github.com/ORIGINAL_OWNER/ORIGINAL_REPOSITORY.git) " originalrepo
-    git remote add upstream "$originalrepo"
-    if $? != 0; then
+    echo -n "Type original repo: (https://github.com/ORIGINAL_OWNER/ORIGINAL_REPOSITORY.git) "
+    read -r originalrepo
+    if git remote add upstream "$originalrepo"; then
+      echo -e "\\e[0;32m✔ \\e[1;30mRemote upstream added\\e[0m";
+    else
       echo -e "✘️ \\e[91mError: \\e[0mFail $ git remote add upstream $originalrepo";
       exit 1;
-    else
-      echo -e "\\e[0;32m✔ \\e[1;30mRemote upstream added\\e[0m";
     fi
   fi
 
-  git checkout "$BRANCH"
-  if [ $? != 0 ]; then
+  if git checkout "$BRANCH"; then
+    echo -e "\\e[0;32m✔ \\e[1;30mCheckout $BRANCH done\\e[0m";
+  else
     echo -e "✘️ \\e[91mError: \\e[0mFail $ git checkout $BRANCH";
     exit 1;
-  else
-    echo -e "\\e[0;32m✔ \\e[1;30mCheckout $BRANCH done\\e[0m";
   fi
 
-  git fetch upstream
-  if [ $? != 0 ]; then
+  if git fetch upstream; then
+    echo -e "\\e[0;32m✔ \\e[1;30mFetch upstream done\\e[0m";
+  else
     echo -e "✘️ \\e[91mError:\\e[0mFail $ git fetch upstream";
     exit 1;
-  else
-    echo -e "\\e[0;32m✔ \\e[1;30mFetch upstream done\\e[0m";
   fi
 
-  git rebase upstream/"$BRANCH" "$BRANCH"
-  if [ $? != 0 ]; then
+  if git rebase upstream/"$BRANCH" "$BRANCH"; then
+    echo -e "\\e[0;32m✔ \\e[1;30mRebase upstream/$BRANCH $BRANCH done\\e[0m";
+  else
     echo -e "✘️ \\e[91mError: \\e[0mFail $ git rebase upstream/$BRANCH $BRANCH";
     exit 1;
-  else
-    echo -e "\\e[0;32m✔ \\e[1;30mRebase upstream/$BRANCH $BRANCH done\\e[0m";
   fi
 
   if $PUSH; then
-    git push
-    if [ $? != 0 ]; then
+    if git push; then
+      echo -e "\\e[0;32m✔ \\e[1;30mPush done\\e[0m";
+    else
       echo -e "✘️ \\e[91mError: \\e[0mFail $ git push";
       exit 1;
-    else
-      echo -e "\\e[0;32m✔ \\e[1;30mPush done\\e[0m";
     fi
   fi
 }
