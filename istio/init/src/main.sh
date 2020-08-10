698423d08226cdab6cbcb46ea18f510853d2d28a
R. A. Souza <alves.wm@gmail.com>
2020-08-10 16:36:53 -0300

    adds double quotes in shell scripts

--- istio/init/src/main.sh
+++ istio/init/src/main.sh
@@ -5,7 +5,7 @@
 . "$(dirname "$0")"/scripts/istioctl.sh --source-only
 #In sh for receive inputs of CLI use: $SAMPLE_TEXT, $SAMPLE_LIST and $SAMPLE_BOOL for this example
 
-runMinikube $MINIKUBE
-runMinikubeConfig $MINIKUBE_CONFIG
-runIstioctl $ISTIOCTL
-runIstioctlInstall $PROFILE
+runMinikube "$MINIKUBE"
+runMinikubeConfig "$MINIKUBE_CONFIG"
+runIstioctl "$ISTIOCTL"
+runIstioctlInstall "$PROFILE"
