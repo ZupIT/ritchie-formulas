ec074c2d08eec415871c8d98a137c23e6d04a3e5
Fabiano Chiareto Fernandes <fabiano.fernandes@zup.com.br>
2020-08-10 23:01:50 -0300

    Beagle Generate Scaffold

---
+++ beagle/generate/scaffold/src/formula/backend-generator/_vendor/beagle-spring/src/main/kotlin/com/base/bff/beagle/project/demo/service/MyService.kt
@@ -0,0 +1,38 @@
+package ${package_name}.service
+
+import br.com.zup.beagle.core.ServerDrivenComponent
+import br.com.zup.beagle.widget.Widget
+import br.com.zup.beagle.widget.action.Action
+import br.com.zup.beagle.widget.action.Alert
+import br.com.zup.beagle.widget.core.TextAlignment
+import br.com.zup.beagle.widget.layout.Screen
+import br.com.zup.beagle.widget.layout.ScreenBuilder
+import br.com.zup.beagle.widget.ui.Text
+import org.springframework.stereotype.Service
+
+@Service
+class MyService {
+    fun createAction(): Action = Alert(
+            title = "My Dialog",
+            message = "This is a native popup!",
+            labelOk= "Close"
+    )
+
+    fun createScreen(): Screen =
+            Screen(child = this.createWidget())
+
+    fun createScreenBuilder(): ScreenBuilder =
+            MyScreenBuilder(this.createWidget())
+
+    fun createWidget(): Widget = Text(
+            text = "Hello, world!",
+            alignment = TextAlignment.CENTER,
+            textColor = "#505050"
+    )
+}
+
+private class MyScreenBuilder(
+        private val component: ServerDrivenComponent
+) : ScreenBuilder {
+    override fun build() = Screen(child = this.component)
+}
---
+++ beagle/generate/scaffold/src/formula/web-generator/_vendor/beagle-react/src/beagle/beagle-service.ts
@@ -0,0 +1,6 @@
+import { createBeagleUIService } from '@zup-it/beagle-react';
+
+export default createBeagleUIService({
+  baseUrl: 'http://localhost:8080',
+  components: {},
+});
