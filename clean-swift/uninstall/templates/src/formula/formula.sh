243bcd32d7190004811b1d57bee4a50104e22cd8
JoaoDanielRufino <joaodaniel0405@gmail.com>
2020-08-13 11:40:38 -0300

    improving UI
    Signed-off-by: JoaoDanielRufino <joaodaniel0405@gmail.com>

--- clean-swift/uninstall/templates/src/formula/formula.sh
+++ clean-swift/uninstall/templates/src/formula/formula.sh
@@ -4,4 +4,12 @@ runFormula() {
   XCODE_USER_TEMPLATES_DIR=~/Library/Developer/Xcode/Templates/File\ Templates
 
   rm -fR "$XCODE_USER_TEMPLATES_DIR"/Clean\ Swift
+
+  retval=$?
+  if [ $retval != 0 ]; then
+    echo "✘️ Fail to uninstall templates";
+    exit 1;
+  else
+    echo "✔ Templates succesfully uninstalled";
+  fi
 }
