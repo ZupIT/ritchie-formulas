23b18ddadb98dae84242c75845dc9f20351886b5
JoaoDanielRufino <joaodaniel0405@gmail.com>
2020-08-13 11:40:38 -0300

    improving UI
    Signed-off-by: JoaoDanielRufino <joaodaniel0405@gmail.com>
    Signed-off-by: RafaelOFreitas <rafa.oliveirafreitas@hotmail.com>

--- clean-swift/install/templates/src/formula/formula.sh
+++ clean-swift/install/templates/src/formula/formula.sh
@@ -1,9 +1,17 @@
-#!/bin/bash
+#!/bin/sh
 
 runFormula() {
   XCODE_USER_TEMPLATES_DIR=~/Library/Developer/Xcode/Templates/File\ Templates
 
-  mkdir -p "$XCODE_USER_TEMPLATES_DIR"
-  rm -fR "$XCODE_USER_TEMPLATES_DIR"/Clean\ Swift
-  cp -R formula/_vendor "$XCODE_USER_TEMPLATES_DIR"
+  mkdir -p "$XCODE_USER_TEMPLATES_DIR" &&
+  rm -fR "$XCODE_USER_TEMPLATES_DIR"/Clean\ Swift &&
+  cp -R formula/_vendor/Clean\ Swift "$XCODE_USER_TEMPLATES_DIR"
+
+  retval=$?
+  if [ $retval != 0 ]; then
+    echo "✘️ Fail to install templates";
+    exit 1;
+  else
+    echo "✔ Templates succesfully installed";
+  fi
 }
