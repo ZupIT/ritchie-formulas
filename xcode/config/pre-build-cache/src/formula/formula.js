af66ba0b992ac14a5fdd959091fd0af428c080cc
JoaoDanielRufino <joaodaniel0405@gmail.com>
2020-08-18 13:34:41 -0300

    removing project name from inputs
    Signed-off-by: JoaoDanielRufino <joaodaniel0405@gmail.com>

--- xcode/config/pre-build-cache/src/formula/formula.js
+++ xcode/config/pre-build-cache/src/formula/formula.js
@@ -15,6 +15,24 @@ async function parse(data) {
   }
 }
 
+async function getProjectName() {
+  const readdir = util.promisify(fs.readdir);
+
+  try {
+    const files = await readdir(process.env.CURRENT_PWD);
+
+    for (let i = 0; i < files.length; i++) {
+      if (files[i].includes('.xcodeproj')) {
+        return files[i].replace('.xcodeproj', '');
+      }
+    }
+  } catch (err) {
+    throw err;
+  }
+
+  throw new Error('.xcodeproj file not found, please cd to your project');
+}
+
 function getPreAction(projectName) {
   return {
     ExecutionAction: {
@@ -26,7 +44,7 @@ function getPreAction(projectName) {
         $: {
           title: 'Run Script',
           scriptText:
-            "# Type a script or drag a script file from your workspace to insert its path.;find ${BUILT_PRODUCTS_DIR} -name '" +
+            "# Type a script or drag a script file from your workspace to insert its path.\nfind ${BUILT_PRODUCTS_DIR} -name '" +
             projectName +
             ".framework' -exec rm -rf {} \\;",
         },
@@ -35,19 +53,21 @@ function getPreAction(projectName) {
   };
 }
 
-async function Run(projectName) {
+async function Run() {
   const readFile = util.promisify(fs.readFile);
   const writeFile = util.promisify(fs.writeFile);
-  const path = `${process.env.CURRENT_PWD}/${projectName}.xcodeproj/xcshareddata/xcschemes/${projectName}.xcscheme`;
 
   try {
+    const projectName = await getProjectName();
+    const path = `${process.env.CURRENT_PWD}/${projectName}.xcodeproj/xcshareddata/xcschemes/${projectName}.xcscheme`;
+
     const file = await readFile(path);
 
     const parsedXML = await parse(file);
 
     if ('PreActions' in parsedXML.Scheme.BuildAction[0]) {
-      parsedXML.Scheme.BuildAction[0].PreActions.push(
-        getPreAction(projectName)
+      parsedXML.Scheme.BuildAction[0].PreActions[0].ExecutionAction.push(
+        getPreAction(projectName).ExecutionAction
       );
     } else {
       parsedXML.Scheme.BuildAction[0].PreActions = getPreAction(projectName);
