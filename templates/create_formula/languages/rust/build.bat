4ff7dda432ccb7d5b2ff5bc02f02dfcb73dfdfa0
Fabiano Chiareto Fernandes <fabiano.fernandes@zup.com.br>
2020-08-19 15:47:53 -0300

    Fix template
    Signed-off-by: Fabiano Chiareto Fernandes <fabiano.fernandes@zup.com.br>

--- templates/create_formula/languages/rust/build.bat
+++ templates/create_formula/languages/rust/build.bat
@@ -1,14 +1,15 @@
 :: Rust parameters
 echo off
 SETLOCAL
+SET BIN_NAME=formula
 SET BIN_FOLDER=bin
-SET BAT_FILE=%BIN_FOLDER%\run.bat
 SET SH_FILE=%BIN_FOLDER%\run.sh
+SET BAT_FILE=%BIN_FOLDER%\run.bat
 :build
     mkdir %BIN_FOLDER%
     xcopy /E /I src %BIN_FOLDER%
     cd %BIN_FOLDER%
-    call cargo install --path .
+    call cargo build --release
     cd ..
     call :BAT_WINDOWS
     call :SH_LINUX
@@ -18,10 +19,11 @@ SET SH_FILE=%BIN_FOLDER%\run.sh
 :BAT_WINDOWS
     echo @ECHO OFF > %BAT_FILE%
     echo SET mypath=%%~dp0 >> %BAT_FILE%
-    echo start /B /WAIT %%mypath:~0,-1%%/target/release/rust.exe >> %BAT_FILE%
+    echo start /B /WAIT %%mypath:~0,-1%%/target/release/%BIN_NAME% >> %BAT_FILE%
 
 :SH_LINUX
-    echo "$(dirname "$0")"/target/release/rust > %SH_FILE%
+    echo "#!/bin/sh" > %SH_FILE%
+    echo "$$(dirname "$$0")"/target/release/%BIN_NAME%  > %SH_FILE%
     GOTO DONE
 
 :CP_DOCKER
