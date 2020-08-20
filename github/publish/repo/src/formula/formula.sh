ee3767d2e00b17480ee182b2861befb20e359bd6
Guillaume <guillaume.falourd@zup.com.br>
2020-08-20 17:40:23 -0300

    [ENHANCEMENT] - RIT GITHUB PUBLISH REPO (#148)
    * merge master
    
    * resolve conflicts
    
    * merge upstream
    
    * removing logs
    
    * updating script and text

--- github/publish/repo/src/formula/formula.sh
+++ github/publish/repo/src/formula/formula.sh
@@ -46,45 +46,45 @@ runFormula() {
 
   if git rev-parse --git-dir > /dev/null 2>&1; then
     echo "🚧 This repository already exists. Preparing new commit..."
-    git add .
-    git commit -m "$VERSION Commit"
+    git add . > /dev/null
+    git commit -m "$VERSION Commit" > /dev/null
   else
-    echo "🚥 Repository creation ($PRIVACY). Preparing first commit..."
+    echo "🚥 Repository creation. Preparing first commit..."
     git init
-      if [[ $DOCKER_EXECUTION ]]; then
-        git config --local user.name $USERNAME
-        git config --local user.email $EMAIL
-      fi
-    git add .
-    git commit -m "Initial Commit"
-    curl -H 'Authorization: token '$TOKEN https://api.github.com/user/repos -d '{"name":"'$slug_name'", "private": '$PRIVACY'}'
+    if [[ $DOCKER_EXECUTION ]]; then
+      git config --local user.name $USERNAME
+      git config --local user.email $EMAIL
+    fi
+    git add . > /dev/null
+    git commit -m "Initial Commit" > /dev/null
+    curl -H 'Authorization: token '$TOKEN https://api.github.com/user/repos -d '{"name":"'$slug_name'", "private": '$PRIVACY'}' > /dev/null
     if [ $? != 0 ]; then
-      echo -e "✘️ \\e[91mError:\\e[0mFail creating $PRIVACY repository";
+      echo -e "✘️ Fail creating new repository";
       exit 1;
     fi
     git remote add origin https://$USERNAME:$TOKEN@github.com/$USERNAME/$slug_name.git
   fi
 
-  git push origin master
+  git push origin master > /dev/null
 
   if [[ $DOCKER_EXECUTION ]]; then
     chown 1000:1000 -R $CURRENT_PWD/$slug_name
   fi
 
   echo "---------------------------------------------------------------------------"
-  echo "✅ Project successfully added on Github!!"
+  echo "✅ Project added on Github"
   echo "📎 Run: $ git clone https://github.com/$USERNAME/$slug_name.git"
   sleep 1s
 
   echo "---------------------------------------------------------------------------"
   echo "🛠 Generating release $VERSION"
   API_JSON=$(printf '{"tag_name": "%s","target_commitish": "master","name": "%s","body": "Release of version %s","draft": false,"prerelease": false}' $VERSION $VERSION $VERSION)
-  curl --data "$API_JSON" https://api.github.com/repos/$USERNAME/$slug_name/releases?access_token=$TOKEN
+  curl --data "$API_JSON" https://api.github.com/repos/$USERNAME/$slug_name/releases?access_token=$TOKEN > /dev/null
   if [ $? != 0 ]; then
-      echo -e "✘️ \\e[91mError:\\e[0mFail generating release $VERSION";
+      echo -e "✘️ Fail generating release $VERSION";
       exit 1;
   fi
-  echo "🚀 Release $VERSION successfully generated !"
+  echo "🚀 Release $VERSION successfully generated"
   sleep 1s
 
   echo "---------------------------------------------------------------------------"
@@ -92,25 +92,25 @@ runFormula() {
   cd ..
   rm -rf ~/.rit/repos/local
   if [ $? != 0 ]; then
-      echo -e "✘️ \\e[91mError:\\e[0mFail removing local build";
+      echo -e "✘️ Fail removing local build";
       exit 1;
   fi
-  echo "🗑 Local build removed successfully !"
+  echo "🗑  Local build removed successfully"
   sleep 1s
 
   echo "---------------------------------------------------------------------------"
-  echo "🐙 Adding https://github.com/$USERNAME/$slug_name Github repository to Ritchie"
+  echo "🐙 Adding Github repository https://github.com/$USERNAME/$slug_name to Ritchie"
   sleep 10s
   echo '{"provider":"Github", "name":"'$slug_name'", "version":"'$VERSION'", "url":"'https://github.com/$USERNAME/$slug_name'", "token":"'$TOKEN'", "priority":2}' | rit add repo --stdin
   if [ $? != 0 ]; then
-      echo -e "✘️ \\e[91mError:\\e[0mFail adding Github repository to Ritchie ($ rit add repo)";
+      echo -e "✘️ Fail adding Github repository to Ritchie ($ rit add repo)";
       exit 1;
   fi
   echo "🔁 Updating Ritchie repository"
   echo '{"name":"'$slug_name'", "version":"'$VERSION'"}' | rit update repo --stdin
   if [ $? != 0 ]; then
-      echo -e "✘️ \\e[91mError:\\e[0mFail updating repository on Github ($ rit update repo)";
+      echo -e "✘️ Fail updating repository on Github ($ rit update repo)";
       exit 1;
   fi
-  echo "👏👏👏 New workspace published successfully !"
+  echo "👏👏👏 New workspace published and imported successfully"
 }
