d5bd5a66813f956787e15fe02944a4a058f7ef9c
GuillaumeFalourd <guillaume.falourd@zup.com.br>
2020-08-20 16:52:17 -0300

    updating files

--- github/delete/repo/src/formula/formula.sh
+++ github/delete/repo/src/formula/formula.sh
@@ -1,7 +1,64 @@
-#!/bin/sh
+#!/bin/bash
+# shellcheck disable=SC2181
+# shellcheck disable=SC2086
+# shellcheck disable=SC2164
+
+removeSpaces() {
+  echo "${1}" | xargs | tr " " -
+}
+
+createSlug() {
+  tmp="$1"
+
+  if [[ "$1" = *" "* ]]; then
+    echo >&2 "Removing spaces from Project name..."
+    tmp=$(removeSpaces "$1")
+    echo >&2 "Project name without spaces: $tmp"
+  fi
+
+  echo "$tmp" | tr '[:upper:]' '[:lower:]'
+}
+
+checkProjectName() {
+  if [[ ! "$1" =~ ^[a-zA-Z0-9-]+$ ]]; then
+    echo "Project name cannot contain special characters"
+    exit 1
+  fi
+}
+
 runFormula() {
-  echo "Hello World! "
-  echo "You receive $SAMPLE_TEXT in text. "
-  echo "You receive $SAMPLE_LIST in list. "
-  echo "You receive $SAMPLE_BOOL in boolean. "
+
+  slug_name=$(createSlug "$PROJECT_NAME")
+  checkProjectName $slug_name
+
+  if [[ $DOCKER_EXECUTION ]]; then
+    git config --local user.name $USERNAME
+  fi
+
+  echo "---------------------------------------------------------------------------"
+  
+  echo "📡 Checking if Github url https://github.com/$USERNAME/$slug_name.git exists."
+
+  git ls-remote "https://github.com/$USERNAME/$slug_name.git" > /dev/null 2>&1
+  
+  if [ "$?" -ne 0 ]; then
+    sleep 1s
+    echo "🚨 Unable to read from https://github.com/$USERNAME/$slug_name.git"
+    exit 1;
+  else
+    echo "🚧 Start deleting project at https://github.com/$USERNAME/$slug_name.git"
+    sleep 1s
+    curl -X DELETE -H 'Authorization: token '$TOKEN https://api.github.com/repos/$USERNAME/$slug_name > /dev/null
+  fi
+  
+  git ls-remote "https://github.com/$USERNAME/$slug_name.git" > /dev/null 2>&1
+
+  if [ "$?" -ne 0 ]; then
+    sleep 1s
+    echo "✅ Project successfully deleted from Github"
+  else
+    sleep 1s
+    echo "🚨 Unable to delete https://github.com/$USERNAME/$slug_name.git repository"
+    echo "🔧 Check your github token authorizations."
+  fi 
 }
