4267f868ac350201e1426362fbd6f8ae48359910
Antonio Oliveira <antonio.filho128@gmail.com>
2020-08-21 20:11:27 -0300

    Wip: Implementing template

---
+++ templates/create_formula/languages/ruby/legacy/template/template.go
@@ -0,0 +1,26 @@
+package template
+
+const (
+	StartFile = "index"
+
+	Index = ``
+
+	Makefile = ``
+
+	Dockerfile = ``
+
+	Run = `#!/bin/sh
+bundle install
+./index.rb`
+
+	Gemfile =
+`source 'https://rubygems.org' do
+	# gem 'nokogiri'
+	# Other gems here
+end`
+
+	File = ``
+
+	WindowsBuild = ``
+)
+
