a928cf6cb67247e46ca2ee5c1d37345c0c8c6968
RafaelOFreitas <rafa.oliveirafreitas@hotmail.com>
2020-08-21 01:11:05 -0300

    Adding formula to automate tool installation

---
+++ environment/install/tools/src/main.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# shellcheck source=/dev/null
+. "$(dirname "$0")"/formula/formula.sh --source-only
+
+
+#In sh for receive inputs of CLI use: $SAMPLE_TEXT, $SAMPLE_LIST and $SAMPLE_BOOL for this example
+
+runFormula "$CONFIGURATION", "$GIT_NAME", "$GIT_EMAIL"
