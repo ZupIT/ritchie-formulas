1da54acc5eb705392230570ae4578c9ca14e5dd0
GuillaumeFalourd <guillaume.falourd@zup.com.br>
2020-08-27 14:52:30 -0300

    update go template to match RIT DEMO HELLO-WORLD formula

--- templates/create_formula/languages/go/src/pkg/formula/formula.go
+++ templates/create_formula/languages/go/src/pkg/formula/formula.go
@@ -1,3 +1,6 @@
+// This is the formula implementation class.
+// Where you will code your methods and manipulate the inputs to perform the specific operation you wish to automate.
+
 package formula
 
 import (
@@ -8,17 +11,28 @@ import (
 )
 
 type Formula struct {
-	Text    string
-	List    string
-	Boolean string
+	Text     string
+	List     string
+	Boolean  bool
+	Password string
 }
 
-func (h Formula) Run(writer io.Writer) {
+func (f Formula) Run(writer io.Writer) {
 	var result string
+
 	result += fmt.Sprintf("Hello world!\n")
-	result += color.FgGreen.Render(fmt.Sprintf("You receive %s in text.\n", h.Text))
-	result += color.FgRed.Render(fmt.Sprintf("You receive %s in list.\n", h.List))
-	result += color.FgYellow.Render(fmt.Sprintf("You receive %s in boolean.\n", h.Boolean))
+
+	result += color.FgGreen.Render(fmt.Sprintf("My name is %s.\n", f.Text))
+
+	if f.Boolean {
+		result += color.FgBlue.Render(fmt.Sprintln("I’ve already created formulas using Ritchie."))
+	} else {
+		result += color.FgRed.Render(fmt.Sprintln("I’m excited in creating new formulas using Ritchie."))
+	}
+
+	result += color.FgYellow.Render(fmt.Sprintf("Today, I want to automate %s.\n", f.List))
+
+	result += color.FgCyan.Render(fmt.Sprintf("My secret is %s.\n", f.Password))
 
 	if _, err := fmt.Fprintf(writer, result); err != nil {
 		panic(err)
