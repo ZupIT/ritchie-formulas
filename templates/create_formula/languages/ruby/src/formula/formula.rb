9b4dc3e81dac38f602de974283c4c8093a6094c4
antoniofilhozup <67597401+antoniofilhozup@users.noreply.github.com>
2020-08-28 00:19:51 -0300

    Feature/template ruby (#159)
    * Wip: Implementing template
    
    * Fix makefile and add Gemfile
    
    * Deleting legacy example files
    
    * Adjusting docker execution
    
    * Makefile adjusts
    
    * Fix Ruby Makefile
    
    Signed-off-by: maurineimirandazup <maurinei.cabral@zup.com.br>
    
    * Final Ruby Makefile
    
    Signed-off-by: maurineimirandazup <maurinei.cabral@zup.com.br>
    
    * Fix double quotes
    
    * Add new line to files
    
    * Fix Docker Image
    
    Signed-off-by: maurineimirandazup <maurinei.cabral@zup.com.br>
    
    Co-authored-by: Antonio Oliveira <antonio.filho128@gmail.com>
    Co-authored-by: maurineimirandazup <maurinei.cabral@zup.com.br>

---
+++ templates/create_formula/languages/ruby/src/formula/formula.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/ruby
+
+def Run(input1, input2, input3)
+    puts "Hello World!"
+    puts "You receive "+ input1 +" in text."
+    puts "You receive "+ input2 +" in list."
+    puts "You receive "+ input3 +" in boolean."
+end
