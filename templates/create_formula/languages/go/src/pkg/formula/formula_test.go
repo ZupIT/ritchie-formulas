537822a2b53cab8748475b76177919009720ae8e
Guillaume <guillaume.falourd@zup.com.br>
2020-08-27 23:18:47 -0300

    [FEATURE] UPDATE HELLO WORLD TEMPLATE (GOLANG) (#160)
    * merge master
    
    * resolve conflicts
    
    * merge upstream
    
    * update go template to match RIT DEMO HELLO-WORLD formula

--- templates/create_formula/languages/go/src/pkg/formula/formula_test.go
+++ templates/create_formula/languages/go/src/pkg/formula/formula_test.go
@@ -10,9 +10,10 @@ import (
 
 func TestFormula_Run(t *testing.T) {
 	type fields struct {
-		Text    string
-		List    string
-		Boolean string
+		Text     string
+		List     string
+		Boolean  bool
+		Password string
 	}
 	tests := []struct {
 		name       string
@@ -20,29 +21,48 @@ func TestFormula_Run(t *testing.T) {
 		wantWriter string
 	}{
 		{
-			name: "Run with success",
+			name: "Run with TRUE",
 			fields: fields{
-				Text:    "Hello",
-				List:    "World",
-				Boolean: "true",
+				Text:     "Dennis",
+				List:     "everything",
+				Boolean:  true,
+				Password: "Ritchie",
 			},
 			wantWriter: func() string {
 				return fmt.Sprintf("Hello world!\n") +
-					color.FgGreen.Render("You receive Hello in text.\n") +
-					color.FgRed.Render("You receive World in list.\n") +
-					color.FgYellow.Render("You receive true in boolean.\n")
+					color.FgGreen.Render(fmt.Sprintf("My name is Dennis.\n")) +
+					color.FgBlue.Render(fmt.Sprintln("I’ve already created formulas using Ritchie.")) +
+					color.FgYellow.Render(fmt.Sprintf("Today, I want to automate everything.\n")) +
+					color.FgCyan.Render(fmt.Sprintf("My secret is Ritchie.\n"))
+			}(),
+		},
+		{
+			name: "Run with FALSE",
+			fields: fields{
+				Text:     "Dennis",
+				List:     "everything",
+				Boolean:  false,
+				Password: "Ritchie",
+			},
+			wantWriter: func() string {
+				return fmt.Sprintf("Hello world!\n") +
+					color.FgGreen.Render(fmt.Sprintf("My name is Dennis.\n")) +
+					color.FgBlue.Render(fmt.Sprintln("I’m excited in creating new formulas using Ritchie.")) +
+					color.FgYellow.Render(fmt.Sprintf("Today, I want to automate everything.\n")) +
+					color.FgCyan.Render(fmt.Sprintf("My secret is Ritchie.\n"))
 			}(),
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			h := Formula{
-				Text:    tt.fields.Text,
-				List:    tt.fields.List,
-				Boolean: tt.fields.Boolean,
+			f := Formula{
+				Text:     tt.fields.Text,
+				List:     tt.fields.List,
+				Boolean:  tt.fields.Boolean,
+				Password: tt.fields.Password,
 			}
 			writer := &bytes.Buffer{}
-			h.Run(writer)
+			f.Run(writer)
 			if gotWriter := writer.String(); gotWriter != tt.wantWriter {
 				t.Errorf("Run() = %v, want %v", gotWriter, tt.wantWriter)
 			}
