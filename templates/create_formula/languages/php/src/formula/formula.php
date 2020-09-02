10c92dedbe6587b730379b03d38a68a25375946b
Henrique Moraes <henrique.rusca@gmail.com>
2020-09-02 20:28:52 -0300

    Adding dependency manager for php
    Signed-off-by: Henrique Moraes <henrique.rusca@gmail.com>
    
    Adding dependency management to php

--- templates/create_formula/languages/php/src/formula/formula.php
+++ templates/create_formula/languages/php/src/formula/formula.php
@@ -1,8 +1,10 @@
 <?php
-	function Run($input1, $input2, $input3) {
-		echo "Hello World! \n";
-		echo "You receive $input1 in text. \n";
-		echo "You receive $input2 in list. \n";
-		echo "You receive $input3 in boolean. \n";
-	}
+use Codedungeon\PHPCliColors\Color;
+
+function Run($input1, $input2, $input3) {
+    echo "Hello World! \n";
+    echo Color::GREEN, "You receive $input1 in text. \n";
+    echo Color::RED, "You receive $input2 in list. \n";
+    echo Color::BLUE, "You receive $input3 in boolean. \n";
+}
 ?>
