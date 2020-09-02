b2dae1baff7ea603e1a3266798c000df2cec32c7
maurineimirandazup <maurinei.cabral@zup.com.br>
2020-09-02 17:56:31 -0300

    Fix Dependencies Install
    Signed-off-by: maurineimirandazup <maurinei.cabral@zup.com.br>

--- templates/create_formula/languages/ruby/build.bat
+++ templates/create_formula/languages/ruby/build.bat
@@ -7,6 +7,7 @@ SET SH_FILE=%BIN_FOLDER%\run.sh
 :build
     mkdir %BIN_FOLDER%
     xcopy /E /I src %BIN_FOLDER%
+    CALL bundle install --gemfile %%BIN_FOLDER%%/Gemfile
     CALL :BAT_WINDOWS
     CALL :SH_LINUX
     CALL :CP_DOCKER
@@ -15,13 +16,11 @@ SET SH_FILE=%BIN_FOLDER%\run.sh
 :BAT_WINDOWS
     echo @ECHO OFF > %BAT_FILE%
     echo SET mypath=%%~dp0 >> %BAT_FILE%
-    echo call bundle install --gemfile %%mypath:~0,-1%%Gemfile >> %BAT_FILE%
     echo ruby %%mypath:~0,-1%%/index.rb >> %BAT_FILE%
     GOTO DONE
 
 :SH_LINUX
     echo #!/bin/sh > %SH_FILE%
-	echo bundle install --quiet --gemfile "$(dirname "$0")"/Gemfile >> %SH_FILE%
 	echo ruby "$(dirname "$0")"/index.rb >> %SH_FILE%
     GOTO DONE
 
