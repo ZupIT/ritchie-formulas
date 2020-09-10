e33e6c72864f09d8b6c713f71cab128c394f6c51
Fabiano Chiareto Fernandes <fabiano.fernandes@zup.com.br>
2020-09-10 10:47:24 -0300

    Fix echo

--- templates/create_formula/languages/powershell/src/formula/formula.ps1
+++ templates/create_formula/languages/powershell/src/formula/formula.ps1
@@ -4,7 +4,7 @@ Function runFormula () {
   $sample_bool = $env:SAMPLE_BOOL
 
   Write-Host "Hello, World!"
-  Write-Host "You receive $sample_text in text. "
+  Write-Host "You receive $sample_text in text."
   Write-Host "You receive $sample_list in list. "
   Write-Host "You receive $sample_bool in boolean. "
 }
