0e14b12ffb196c020afb120b0ec7f74faf26e6af
Fabiano Chiareto Fernandes <fabiano.fernandes@zup.com.br>
2020-09-10 12:53:19 -0300

    Add docker execution
    Signed-off-by: Fabiano Chiareto Fernandes <fabiano.fernandes@zup.com.br>

--- aws/update/kubeconfig/src/formula/formula.sh
+++ aws/update/kubeconfig/src/formula/formula.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 runFormula() {
+  if [[ $DOCKER_EXECUTION ]] ; then
+    mkdir -p /root/.aws/
+    echo "[default]" > /root/.aws/credentials
+    echo "aws_access_key_id=$ACCESS_KEY" >> /root/.aws/credentials
+    echo "aws_secret_access_key=$SECRET_ACCESS_KEY" >> /root/.aws/credentials
+  fi
+  echo "Running: aws eks --region $REGION update-kubeconfig --name $CLUSTER_NAME ..."
   aws eks --region "$REGION" update-kubeconfig --name "$CLUSTER_NAME"
 }
