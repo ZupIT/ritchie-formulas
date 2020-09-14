96092f19453b0c00304eb42681847f0610fa58aa
Fabiano Chiaretto Fernandes <58859234+fabianofernandeszup@users.noreply.github.com>
2020-09-14 16:58:47 -0300

    Template PowerShell (#174)
    * Template PowerShell
    
    
    * Fix run and build with docker on linux debian
    
    
    * Fix pre-commit
    
    
    * Fix echo

---
+++ templates/create_formula/languages/powershell/src/formula/formula.ps1
@@ -0,0 +1,10 @@
+Function runFormula () {
+  $sample_text = $env:SAMPLE_TEXT
+  $sample_list = $env:SAMPLE_LIST
+  $sample_bool = $env:SAMPLE_BOOL
+
+  Write-Host "Hello, World!"
+  Write-Host "You receive $sample_text in text."
+  Write-Host "You receive $sample_list in list. "
+  Write-Host "You receive $sample_bool in boolean. "
+}
