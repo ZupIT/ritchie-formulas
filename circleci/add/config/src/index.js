08488d3c5bee9fe00472428f9865156f8d53b982
Henrique Moraes <henrique.rusca@gmail.com>
2020-09-21 12:39:33 -0300

    Removing config formula
    Signed-off-by: Henrique Moraes <henrique.rusca@gmail.com>

--- circleci/add/config/src/index.js
+++
@@ -1,9 +0,0 @@
-const run = require("./formula/formula");
-
-run(
-    process.env.PROJECT_NAME,
-    process.env.DATABASE,
-    process.env.REGISTRY,
-    process.env.STAGING_DEPLOY,
-    process.env.PRODUCTION_DEPLOY
-);
