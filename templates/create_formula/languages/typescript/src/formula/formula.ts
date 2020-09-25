dbc39115525efd43befb0268a71a852bc5d74088
Bruno N. Melo <brunonobrega.melo@gmail.com>
2020-09-25 16:04:42 -0300

    update and fix imports
    Signed-off-by: Bruno N. Melo <brunonobrega.melo@gmail.com>

--- templates/create_formula/languages/typescript/src/formula/formula.ts
+++ templates/create_formula/languages/typescript/src/formula/formula.ts
@@ -1,10 +1,10 @@
-import clc from "cli-color"
+import * as chalk from 'chalk'
 
 function Run(input1: string, input2: string, input3: boolean) {
-    console.log("Hello World!")
-    console.log(clc.green(`You receive ${input1} in text.`));
-    console.log(clc.red(`You receive ${input2} in list.`));
-    console.log(clc.yellow(`You receive ${input3} in boolean.`));
+    console.log('Hello World!')
+    console.log(chalk.green(`You receive ${input1} in text.`));
+    console.log(chalk.red(`You receive ${input2} in list.`));
+    console.log(chalk.yellow(`You receive ${input3} in boolean.`));
 }
 
 const formula = Run
