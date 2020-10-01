72c379630e8d39992d3c8f7ff41fee81646430dc
Anthony Jones <anthony@wildinterior.co>
2020-10-01 15:18:02 -0400

    Update Rust template to match hello-world formula (#250)
    Using the hello-world formula as a guideline
    (https://github.com/ZupIT/ritchie-formulas-demo/tree/master/demo/hello-world):
    this commit updates the Rust template to show four ritchie input methods and match the language and colors.

--- templates/create_formula/languages/rust/src/src/formula/mod.rs
+++ templates/create_formula/languages/rust/src/src/formula/mod.rs
@@ -1,8 +1,21 @@
 use colored::*;
 
-pub fn run(sample_text: String, sample_list: String, sample_bool: String) {
+pub fn run(input_text: String, input_bool: bool, input_list: String, input_password: String) {
     println!("Hello World!");
-    println!("{}", format!("You receive {} in text.", sample_text).blue());
-    println!("{}", format!("You receive {} in list.", sample_list).green());
-    println!("{}", format!("You receive {} in boolean.", sample_bool).red());
+    println!("{}", format!("My name is {}.", input_text).green());
+
+    if input_bool {
+        println!("{}", "I’ve already created formulas using Ritchie.".red())
+    } else {
+        println!(
+            "{}",
+            "I’m excited in creating new formulas using Ritchie.".red()
+        )
+    }
+
+    println!(
+        "{}",
+        format!("Today, I want to automate {}.", input_list).yellow()
+    );
+    println!("{}", format!("My secret is {}.", input_password).cyan());
 }
