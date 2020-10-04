2fd68ed8672efbcfa469919a982fc66b551c764b
Anthony Jones <anthony@beforeman.co>
2020-10-04 12:24:34 -0400

    Update Shell template to match hello-world formula
    Using the hello-world formula as a guideline
    (https://github.com/ZupIT/ritchie-formulas-demo/tree/master/demo/hello-world):
    this commit updates the shell template to show four ritchie input methods and match the language and colors.
    
    Signed-off-by: Anthony Jones <anthony@beforeman.co>

--- templates/create_formula/languages/shell/src/main.sh
+++ templates/create_formula/languages/shell/src/main.sh
@@ -4,4 +4,4 @@
 . "$(dirname "$0")"/formula/formula.sh --source-only
 #In sh for receive inputs of CLI use: $SAMPLE_TEXT, $SAMPLE_LIST and $SAMPLE_BOOL for this example
 
-runFormula "$SAMPLE_TEXT" "$SAMPLE_LIST" "$SAMPLE_BOOL"
+runFormula "$INPUT_TEXT" "$INPUT_BOOLEAN" "$INPUT_LIST" "$INPUT_PASSWORD"
