3b622acac8ead3980c5b55c4505fcf08c66a060f
Prateek Saini <pratiiksaini@gmail.com>
2020-10-04 13:53:03 +0530

    Update Node.js template

--- templates/create_formula/languages/node/src/index.js
+++ templates/create_formula/languages/node/src/index.js
@@ -1,7 +1,10 @@
-const run = require("./formula/formula")
+var path = require('path'),
+run = require("./formula/formula"),
+config_data = require(path.resolve('../config.json'));
 
-const INPUT1 = process.env.SAMPLE_TEXT
-const INPUT2 = process.env.SAMPLE_LIST
-const INPUT3 = process.env.SAMPLE_BOOL
+const INPUT1 = process.env.INPUT_TEXT||config_data.inputs[0].name
+const INPUT2 = process.env.INPUT_BOOLEAN||config_data.inputs[1].name
+const INPUT3 = process.env.INPUT_LIST||config_data.inputs[2].name
+const INPUT4 = process.env.INPUT_PASSWORD||config_data.inputs[3].name
 
-run(INPUT1, INPUT2, INPUT3)
+run(INPUT1, INPUT2, INPUT3, INPUT4)
