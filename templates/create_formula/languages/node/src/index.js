8fca30581f53c4e947644b6e461098cd2c4935ea
Prateek Saini <pratiiksaini@gmail.com>
2020-10-05 20:24:51 +0530

    Update config.js and index.js

--- templates/create_formula/languages/node/src/index.js
+++ templates/create_formula/languages/node/src/index.js
@@ -2,9 +2,9 @@ var path = require('path'),
 run = require("./formula/formula"),
 config_data = require(path.resolve('../config.json'));
 
-const INPUT1 = process.env.INPUT_TEXT||config_data.inputs[0].name
-const INPUT2 = process.env.INPUT_BOOLEAN||config_data.inputs[1].name
-const INPUT3 = process.env.INPUT_LIST||config_data.inputs[2].name
-const INPUT4 = process.env.INPUT_PASSWORD||config_data.inputs[3].name
+const INPUT1 = process.env.INPUT_TEXT
+const INPUT2 = process.env.INPUT_BOOLEAN
+const INPUT3 = process.env.INPUT_LIST
+const INPUT4 = process.env.INPUT_PASSWORD
 
 run(INPUT1, INPUT2, INPUT3, INPUT4)
