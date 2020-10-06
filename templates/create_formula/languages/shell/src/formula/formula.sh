7a71d6d718d6674d2b49c9adc5f438f590413fa9
Anthony Jones <anthony@beforeman.co>
2020-10-06 09:36:18 -0400

    Update Shell template to match hello-world formula (#263)
    Using the hello-world formula as a guideline
    (https://github.com/ZupIT/ritchie-formulas-demo/tree/master/demo/hello-world):
    this commit updates the shell template to show four ritchie input methods and match the language and colors.

--- templates/create_formula/languages/shell/src/formula/formula.sh
+++ templates/create_formula/languages/shell/src/formula/formula.sh
@@ -1,7 +1,36 @@
 #!/bin/sh
+
 runFormula() {
   echo "Hello World! "
-  echo "You receive $SAMPLE_TEXT in text. "
-  echo "You receive $SAMPLE_LIST in list. "
-  echo "You receive $SAMPLE_BOOL in boolean. "
+  echoColor "green" "My name is $1."
+
+  if [ "$2" = "true" ]; then
+    echoColor "blue" "I've already created formulas using Ritchie."
+  else
+    echoColor "red" "I'm excited in creating new formulas using Ritchie."
+  fi
+
+  echoColor "yellow" "Today, I want to automate $3."
+  echoColor "cyan"  "My secret is $4."
 }
+
+echoColor() {
+  case $1 in
+    red)
+      echo "$(printf '\033[31m')$2$(printf '\033[0m')"
+      ;;
+    green)
+      echo "$(printf '\033[32m')$2$(printf '\033[0m')"
+      ;;
+    yellow)
+      echo "$(printf '\033[33m')$2$(printf '\033[0m')"
+      ;;
+    blue)
+      echo "$(printf '\033[34m')$2$(printf '\033[0m')"
+      ;;
+    cyan)
+      echo "$(printf '\033[36m')$2$(printf '\033[0m')"
+      ;;
+    esac
+}
+
