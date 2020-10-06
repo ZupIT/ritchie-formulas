91c1f6c4c37f5f657aaeb76b8166a373f3565d6d
Maurinei Cabral <67597621+maurineimirandazup@users.noreply.github.com>
2020-10-06 10:38:11 -0300

    Fix - Individual Build (#264)
    Signed-off-by: maurineimirandazup <maurinei.miranda@zup.com.br>

--- templates/create_formula/languages/rust/build.bat
+++ templates/create_formula/languages/rust/build.bat
@@ -21,11 +21,6 @@ SET BAT_FILE=%BIN_FOLDER%\run.bat
     echo SET mypath=%%~dp0 >> %BAT_FILE%
     echo start /B /WAIT %%mypath:~0,-1%%/target/release/%BIN_NAME% >> %BAT_FILE%
 
-:SH_LINUX
-    echo "#!/bin/sh" > %SH_FILE%
-    echo "$$(dirname "$$0")"/target/release/%BIN_NAME%  > %SH_FILE%
-    GOTO DONE
-
 :CP_DOCKER
     copy Dockerfile %BIN_FOLDER%
     copy set_umask.sh %BIN_FOLDER%
