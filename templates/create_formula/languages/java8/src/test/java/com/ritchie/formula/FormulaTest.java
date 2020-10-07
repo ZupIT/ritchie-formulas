d003e656fb2670d2a181cfd56f3a8d9d103fed85
Bruno N. Melo <brunonobrega.melo@gmail.com>
2020-10-07 16:54:42 -0300

    update template on java8
    Signed-off-by: Bruno N. Melo <brunonobrega.melo@gmail.com>

--- templates/create_formula/languages/java8/src/test/java/com/ritchie/formula/FormulaTest.java
+++ templates/create_formula/languages/java8/src/test/java/com/ritchie/formula/FormulaTest.java
@@ -1,19 +1,59 @@
 package com.ritchie.formula;
 
-import static org.junit.Assert.*;
-
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertEquals;
 
 public class FormulaTest {
 
+  private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+  private final PrintStream originalOut = System.out;
+
+  @Before
+  public void setUpStreams() {
+    System.setOut(new PrintStream(outContent));
+  }
+
+  @After
+  public void restoreStreams() {
+    System.setOut(originalOut);
+  }
+
   @Test
-  public void run() {
-    Formula formula = new Formula("Hello", "World", true);
-    String excpeted =
-        "Hello World!\n"
-            + "You receive Hello in text.\n"
-            + "You receive World in list.\n"
-            + "You receive true in boolean.\n";
-    assertEquals(excpeted, formula.Run());
+  public void runTrueInput() {
+    new Formula("Hello", true, "world", "pass").Run();
+
+    assertEquals("Hello World!\n" +
+            "\u001B[32mMy name is Hello.\u001B[39m\n" +
+            "\u001B[34mI’ve already created formulas using Ritchie.\u001B[39m\n" +
+            "\u001B[33mToday, I want to automate world.\u001B[39m\n" +
+            "\u001B[36mMy secret is pass.\u001B[39m\n", outContent.toString());
+  }
+
+  @Test
+  public void runFalseInput() {
+    new Formula("Hello", false, "world", "pass").Run();
+
+    assertEquals("Hello World!\n" +
+            "\u001B[32mMy name is Hello.\u001B[39m\n" +
+            "\u001B[31mI’m excited in creating new formulas using Ritchie.\u001B[39m\n" +
+            "\u001B[33mToday, I want to automate world.\u001B[39m\n" +
+            "\u001B[36mMy secret is pass.\u001B[39m\n", outContent.toString());
+  }
+
+  @Test
+  public void runNoSecresInput() {
+    new Formula("Hello", false, "world", "").Run();
+
+    assertEquals("Hello World!\n" +
+            "\u001B[32mMy name is Hello.\u001B[39m\n" +
+            "\u001B[31mI’m excited in creating new formulas using Ritchie.\u001B[39m\n" +
+            "\u001B[33mToday, I want to automate world.\u001B[39m\n" +
+            "\u001B[36mMy secret is .\u001B[39m\n", outContent.toString());
   }
 }
