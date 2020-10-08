b2ee7e2ba92dbf5c6edce5703f413a27d1c910f7
maurinei-miranda <maurinei.develop@gmail.com>
2020-10-08 12:26:53 -0300

    Update inputs and add colors
    Signed-off-by: maurinei-miranda <maurinei.develop@gmail.com>

--- templates/create_formula/languages/csharp/src/formula/formula.cs
+++ templates/create_formula/languages/csharp/src/formula/formula.cs
@@ -1,22 +1,34 @@
 using System;
-using main;
 
 namespace formula
 {
     public class Hello
     {
-        public string Input1;
-        public string Input2;
-        public string Input3;
-        public Hello(string sptx, string sptl, string sptb)
+        public string input1;
+        public string input2;
+        public string input3;
+        public string input4;
+        public Hello(string intext, string inlist, string inbool, string insecret)
         {
-            this.Input1 = sptx;
-            this.Input2 = sptl;
-            this.Input3 = sptb;
+            this.input1 = intext;
+            this.input2 = inlist;
+            this.input3 = inbool;
+            this.input4 = insecret;
+
             Console.WriteLine("Hello World!");
-            Console.WriteLine("You receive " + Input1 + " in text. ");
-            Console.WriteLine("You receive " + Input2 + " in list. ");
-            Console.WriteLine("You receive " + Input3 + " in boolean. ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"My name is {input1}");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            if (input3 == "true")   {
+                Console.WriteLine("I’ve already created formulas using Ritchie.");
+            } else  {
+                Console.WriteLine("I’m excited in creating new formulas using Ritchie.");
+            }
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"Today, I want to automate {input2}");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine($"My secret is {input4}");
+            Console.ResetColor();
         }
 
     }
