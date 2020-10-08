ff123ef1334c508c18958252088ead4fd47d295d
João Daniel Rufino <joaodaniel0405@gmail.com>
2020-10-08 14:54:26 -0300

    [Improvement] - Windows support to templates (#247)
    * windows support
    
    Signed-off-by: JoaoDanielRufino <joaodaniel0405@gmail.com>
    
    * Build.bat with Docker
    
    Signed-off-by: maurineimirandazup <maurinei.miranda@zup.com.br>
    
    * Fix Build.bat
    
    Signed-off-by: maurineimirandazup <maurinei.miranda@zup.com.br>
    
    * Fix Make file
    
    Signed-off-by: maurineimirandazup <maurinei.miranda@zup.com.br>
    
    * adding end of line
    
    Signed-off-by: JoaoDanielRufino <joaodaniel0405@gmail.com>
    
    * update output
    
    Signed-off-by: JoaoDanielRufino <joaodaniel0405@gmail.com>
    
    Co-authored-by: maurineimirandazup <maurinei.miranda@zup.com.br>

--- templates/create_formula/languages/shell-bat/src/main.sh
+++ templates/create_formula/languages/shell-bat/src/main.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # shellcheck source=/dev/null
-. "$(dirname "$0")"/formula/formula.sh --source-only
+. "$(dirname "$0")"/unix/formula/formula.sh --source-only
 #In sh for receive inputs of CLI use: $SAMPLE_TEXT, $SAMPLE_LIST and $SAMPLE_BOOL for this example
 
-runFormula "$INPUT_TEXT" "$INPUT_BOOLEAN" "$INPUT_LIST" "$INPUT_PASSWORD"
+runFormula
