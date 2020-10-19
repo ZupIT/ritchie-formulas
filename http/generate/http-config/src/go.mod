19881f28de524e8ae81c5d8f013b85b5d71f1593
Douglas Winter Branco <douglasvinter@gmail.com>
2020-10-19 09:04:34 -0300

    [Feature] Formulas for http performance test: http-load & http-config (#282)
    * [Feature] Formulas for http performance test: http-load & http-config
    
    
    * [Feature] Added gif on README.md for each formula + Code Lint

---
+++ http/generate/http-config/src/go.mod
@@ -0,0 +1,8 @@
+module template
+
+go 1.14
+
+require (
+	github.com/fatih/color v1.9.0
+	github.com/gookit/color v1.2.5
+)
---
+++ http/generate/http-config/src/main.go
@@ -0,0 +1,12 @@
+// This is the main class.
+// Where you will extract the inputs asked on the config.json file and call the formula's method(s).
+
+package main
+
+import (
+	"template/pkg/template"
+)
+
+func main() {
+	template.Run()
+}
