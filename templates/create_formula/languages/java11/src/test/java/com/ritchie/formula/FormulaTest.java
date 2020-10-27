ec016e2cee24873f8d9df4ee2c904fb10fae5e7e
Fabiano Chiareto Fernandes <fabiano.fernandes@zup.com.br>
2020-10-27 14:19:34 -0300

    Fix inputs and outputs template java11
    Signed-off-by: Fabiano Chiareto Fernandes <fabiano.fernandes@zup.com.br>

--- templates/create_formula/languages/java11/src/test/java/com/ritchie/formula/FormulaTest.java
+++ templates/create_formula/languages/java11/src/test/java/com/ritchie/formula/FormulaTest.java
@@ -26,33 +26,33 @@ public class FormulaTest {
 
   @Test
   public void runTrueInput() {
-    new Formula("Hello", "world", "pass", true).Run();
+    new Formula("Hello", true, "world", "pass").Run();
+
     assertEquals("Hello World!\n" +
             "\u001B[32mMy name is Hello.\u001B[39m\n" +
-            "\u001B[36mI’ve already created formulas using Ritchie.\u001B[39m\n" +
+            "\u001B[36mI've already created formulas using Ritchie.\u001B[39m\n" +
             "\u001B[33mToday, I want to automate world.\u001B[39m\n" +
             "\u001B[35mMy secret is pass.\u001B[39m\n", outContent.toString());
   }
-
 
   @Test
   public void runFalseInput() {
-    new Formula("Hello", "world", "pass", false).Run();
+    new Formula("Hello", false, "world", "pass").Run();
 
     assertEquals("Hello World!\n" +
             "\u001B[32mMy name is Hello.\u001B[39m\n" +
-            "\u001B[31mI’m excited in creating new formulas using Ritchie.\u001B[39m\n" +
+            "\u001B[31mI'm excited in creating new formulas using Ritchie.\u001B[39m\n" +
             "\u001B[33mToday, I want to automate world.\u001B[39m\n" +
             "\u001B[35mMy secret is pass.\u001B[39m\n", outContent.toString());
   }
 
   @Test
-  public void runNoSecretsInput() {
-    new Formula("Hello", "world", "", false).Run();
+  public void runNoSecresInput() {
+    new Formula("Hello", false, "world", "").Run();
 
     assertEquals("Hello World!\n" +
             "\u001B[32mMy name is Hello.\u001B[39m\n" +
-            "\u001B[31mI’m excited in creating new formulas using Ritchie.\u001B[39m\n" +
+            "\u001B[31mI'm excited in creating new formulas using Ritchie.\u001B[39m\n" +
             "\u001B[33mToday, I want to automate world.\u001B[39m\n" +
             "\u001B[35mMy secret is .\u001B[39m\n", outContent.toString());
   }
