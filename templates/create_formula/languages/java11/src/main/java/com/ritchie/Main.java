7d165bfaff8761d2572211cd3c234b493d34d460
Fabiano Chiaretto Fernandes <58859234+fabianofernandeszup@users.noreply.github.com>
2020-10-27 14:50:08 -0300

    Fix inputs and outputs template java11 (#291)
    * Fix inputs and outputs template java11
    
    
    * Fix method name runNoSecresInput to runNoSecretsInput

--- templates/create_formula/languages/java11/src/main/java/com/ritchie/Main.java
+++ templates/create_formula/languages/java11/src/main/java/com/ritchie/Main.java
@@ -3,12 +3,15 @@ package com.ritchie;
 import com.ritchie.formula.Formula;
 
 public class Main {
+
   public static void main(String[] args) {
-    String input_text = System.getenv("INPUT_TEXT");
-    String input_list = System.getenv("INPUT_LIST");
-    String password = System.getenv("PASSWORD");
-    boolean input_boolean = Boolean.parseBoolean(System.getenv("INPUT_BOOLEAN"));
-    Formula formula = new Formula(input_text, input_list, password, input_boolean);
+
+    String inputText = System.getenv("INPUT_TEXT");
+    boolean inputBoolean = Boolean.parseBoolean(System.getenv("INPUT_BOOLEAN"));
+    String inputList = System.getenv("INPUT_LIST");
+    String inputPassword = System.getenv("INPUT_PASSWORD");
+
+    Formula formula = new Formula(inputText, inputBoolean, inputList, inputPassword);
     formula.Run();
   }
 }
