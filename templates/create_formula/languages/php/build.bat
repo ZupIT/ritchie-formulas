13b60c8a529ead2d3980bae85c6473551c269cda
Fabiano Chiareto Fernandes <fabiano.fernandes@zup.com.br>
2020-10-31 14:29:09 -0300

    Update template php

--- templates/create_formula/languages/php/build.bat
+++ templates/create_formula/languages/php/build.bat
@@ -4,6 +4,7 @@ SETLOCAL
 SET BIN_FOLDER=bin
 SET BAT_FILE=%BIN_FOLDER%\run.bat
 SET SH_FILE=%BIN_FOLDER%\run.sh
+
 :build
     mkdir %BIN_FOLDER%
     xcopy /E /I src %BIN_FOLDER%
@@ -19,11 +20,12 @@ SET SH_FILE=%BIN_FOLDER%\run.sh
     echo @ECHO OFF > %BAT_FILE%
     echo SET mypath=%%~dp0 >> %BAT_FILE%
     echo start /B /WAIT php -f %%mypath:~0,-1%%/index.php >> %BAT_FILE%
-
-:SH_LINUX
-    echo php -f "$(dirname "$0")"/index.php > %SH_FILE%
     GOTO DONE
 
+:SH_LINUX
+    echo #!/bin/sh > %SH_FILE%
+    echo php -f "$(dirname "$0")"/index.php > %SH_FILE%
+    GOTO DONE
 
 :CP_DOCKER
     copy Dockerfile %BIN_FOLDER%
