1abaa86e55e8bb00d5b9edcb04989d4b1b072d3f
Fernando Ike <fernando.ike@gmail.com>
2020-10-31 16:01:54 -0300

    Remove files reached for pre-commit.sh
    Signed-off-by: Fernando Ike <fernando.ike@gmail.com>

--- templates/create_formula/languages/python3/src/formula/formula.py
+++ templates/create_formula/languages/python3/src/formula/formula.py
@@ -2,8 +2,14 @@
 from colored import fg, attr
 
 
-def Run(input1, input2, input3):
+def Run(input1, input2, input3, input4):
     print("Hello World!")
-    print("%sYou receive {} in text.%s".format(input1) % (fg(2), attr(0)))
-    print("%sYou receive {} in list.%s".format(input2) % (fg(1), attr(0)))
-    print("%sYou receive {} in boolean.%s".format(input3) % (fg(3), attr(0)))
+    print("%sMy name is {}.%s".format(input1) % (fg(2), attr(0)))
+    if input2:
+        print("%sI've already created formulas using Ritchie.%s" % (fg(3), attr(0)))
+    else:
+        print(
+            "%sI'm excited in creating new formulas using Ritchie.%s" % (fg(3), attr(0))
+        )
+    print("%sToday, I want to automate {}.%s".format(input3) % (fg(1), attr(0)))
+    print("%sMy secret is {}.%s".format(input4) % (fg(3), attr(0)))
