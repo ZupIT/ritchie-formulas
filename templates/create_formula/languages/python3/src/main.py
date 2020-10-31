24891ae51fc046aaac7b769c9ed4b51ee81c030b
Fabiano Chiareto Fernandes <fabiano.fernandes@zup.com.br>
2020-10-31 14:55:44 -0300

    Update template python

--- templates/create_formula/languages/python3/src/main.py
+++ templates/create_formula/languages/python3/src/main.py
@@ -3,7 +3,8 @@ import os
 
 from formula import formula
 
-input1 = os.environ.get("SAMPLE_TEXT")
-input2 = os.environ.get("SAMPLE_LIST")
-input3 = os.environ.get("SAMPLE_BOOL")
-formula.Run(input1, input2, input3)
+input1 = os.environ.get("INPUT_TEXT")
+input2 = os.environ.get("INPUT_LIST")
+input3 = os.environ.get("INPUT_BOOL")
+input4 = os.environ.get("INPUT_PASSWORD")
+formula.Run(input1, input2, input3, input4)
