93819d2730fc9f37bd03b9d2e618fd83e261330d
Fernando ike <fike@maburix.com>
2020-10-31 17:38:18 -0300

    Updated python3 template (#298)
    * Remove files reached for pre-commit.sh
    
    Signed-off-by: Fernando Ike <fernando.ike@gmail.com>
    
    * added boolean verification, thanks fabiano for your help
    
    Signed-off-by: Fernando Ike <fernando.ike@gmail.com>

--- templates/create_formula/languages/python3/src/formula/formula.py
+++ templates/create_formula/languages/python3/src/formula/formula.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
 from colored import fg, attr
+from distutils.util import strtobool
 
-
-def Run(input1, input2, input3):
+def Run(input1, input2, input3, input4):
     print("Hello World!")
-    print("%sYou receive {} in text.%s".format(input1) % (fg(2), attr(0)))
-    print("%sYou receive {} in list.%s".format(input2) % (fg(1), attr(0)))
-    print("%sYou receive {} in boolean.%s".format(input3) % (fg(3), attr(0)))
+    print("%sMy name is {}.%s".format(input1) % (fg(2), attr(0)))
+    if (bool(strtobool(input2))):
+        print("%sI've already created formulas using Ritchie.%s" % (fg(3), attr(0)))
+    else:
+        print(
+            "%sI'm excited in creating new formulas using Ritchie.%s" % (fg(3), attr(0))
+        )
+    print("%sToday, I want to automate {}.%s".format(input3) % (fg(1), attr(0)))
+    print("%sMy secret is {}.%s".format(input4) % (fg(3), attr(0)))
