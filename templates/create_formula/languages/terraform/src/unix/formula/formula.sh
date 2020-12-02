097a8e322e5c7af47dcdb340f5c0a272e2bdbf3b
maurineimirandazup <maurinei.miranda@zup.com.br>
2020-12-02 14:11:15 -0300

    Terraform Formula Template
    Signed-off-by: maurineimirandazup <maurinei.miranda@zup.com.br>

---
+++ templates/create_formula/languages/terraform/src/unix/formula/formula.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+runFormula() {
+  if [ -f /.dockerenv ] ; then
+    cd /rit/tf-files
+    export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+  else
+    cd tf-files
+  fi
+
+  # Export TF VAR to Uppercase
+  for e in $(printenv); do
+    if [[ $e == "TF_VAR_"* ]]; then
+      env_value=$(cut -d'=' -f2 <<< "$e")
+      env_name=$(cut -d'=' -f1 <<<"$e")
+      sub_env_name_lower=$(echo "$env_name" | cut -c8-| tr '[:upper:]' '[:lower:]')
+      export $(echo "TF_VAR_${sub_env_name_lower}=${env_value}")
+    fi
+  done
+
+  terraform version
+
+
+
+  if [ "$BACKEND_S3" != "" ]; then
+    echo "terraform {" >> main.tf
+    echo "  backend \"s3\" {}" >> main.tf
+    echo "}" >> main.tf
+
+    cat main.tf
+
+    terraform init \
+    -backend-config="key=terraform.tfstate" \
+    -backend-config="bucket=$BACKEND_S3" \
+    -backend-config="region=$TF_VAR_BUCKET_REGION"
+  else
+    terraform init
+  fi
+
+  if [[ $ACTION == "plan" ]]; then
+    terraform plan
+  else
+    terraform $ACTION -auto-approve
+  fi
+}
