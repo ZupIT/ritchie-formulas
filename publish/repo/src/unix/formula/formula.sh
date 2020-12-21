044f14e82ca2ba3bb376881d320c6a4afefda278
Guillaume Falourd <guillaume.falourd@zup.com.br>
2020-12-21 09:24:20 -0300

    Fix/rit publish repo formula (stdin to input flag) (#326)
    * merge master
    
    * resolve conflicts
    
    * merge upstream
    
    * update from STDIN to INPUT FLAG
    
    Signed-off-by: GuillaumeFalourd <guillaume.falourd@zup.com.br>
    
    * update the .bat file
    
    Signed-off-by: GuillaumeFalourd <guillaume.falourd@zup.com.br>

--- publish/repo/src/unix/formula/formula.sh
+++ publish/repo/src/unix/formula/formula.sh
@@ -7,11 +7,13 @@ runFormula() {
   if [ "Github" == $PROVIDER ]
   then
     echo "🐙 Github provider selected"
-    echo '{"privacy":"'$PRIVACY'", "project_name":"'$PROJECT_NAME'", "workspace_path":"'$WORKSPACE_PATH'", "version":"'$VERSION'"}' | rit github publish repo --stdin
+#    echo '{"privacy":"'$PRIVACY'", "project_name":"'$PROJECT_NAME'", "workspace_path":"'$WORKSPACE_PATH'", "version":"'$VERSION'"}' | rit github publish repo --stdin
+    rit github publish repo --privacy=$PRIVACY --project_name=$PROJECT_NAME --workspace_path=$WORKSPACE_PATH --version=$VERSION
   elif [ "Gitlab" == $PROVIDER ]
   then
     echo "🦊 Gitlab provider selected"
-    echo '{"privacy":"'$PRIVACY'", "project_name":"'$PROJECT_NAME'", "workspace_path":"'$WORKSPACE_PATH'", "version":"'$VERSION'"}' | rit gitlab publish repo --stdin
+#    echo '{"privacy":"'$PRIVACY'", "project_name":"'$PROJECT_NAME'", "workspace_path":"'$WORKSPACE_PATH'", "version":"'$VERSION'"}' | rit gitlab publish repo --stdin
+    rit gitlab publish repo --privacy=$PRIVACY --project_name=$PROJECT_NAME --workspace_path=$WORKSPACE_PATH --version=$VERSION
   else
     echo "🤖 Unexpected Provider informed. Check it please and try again."
   fi
