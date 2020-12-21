9faeaaac11876bebe62c9cbb8e206d708b8e8664
Fabiano Chiaretto Fernandes <58859234+fabianofernandeszup@users.noreply.github.com>
2020-12-21 09:25:49 -0300

    Formulas Yourls (#332)
    * Formulas NetoMarin
    
    
    * Fix pre-commit issues
    
    
    * Fix pre-commit issues
    
    
    * Fix horus issues

---
+++ yourls/short/url/src/test/java/com/ritchie/formula/FormulaTest.java
@@ -0,0 +1,15 @@
+package com.ritchie.formula;
+
+import static org.junit.Assert.*;
+
+import org.json.simple.parser.ParseException;
+import org.junit.Test;
+
+import java.io.IOException;
+
+public class FormulaTest {
+
+  @Test
+  public void run() throws InterruptedException, ParseException, IOException {
+  }
+}
