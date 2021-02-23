9685b5b7d53097a472e2768fa7413e69deaf7390
Fabiano Chiareto Fernandes <fabiano.fernandes@zup.com.br>
2021-02-23 10:27:23 -0300

    Fix build templates shell/bat and terraform.

--- templates/create_formula/languages/terraform/src/main.sh
+++ templates/create_formula/languages/terraform/src/main.sh
@@ -2,6 +2,5 @@
 
 # shellcheck source=/dev/null
 . "$(dirname "$0")"/unix/formula/formula.sh --source-only
-#In sh for receive inputs of CLI use: $SAMPLE_TEXT, $SAMPLE_LIST and $SAMPLE_BOOL for this example
 
 runFormula
