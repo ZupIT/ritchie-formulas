a349a6b0b1469887a1db5a7eeda1ecec03e16908
João Daniel Rufino <joaodaniel0405@gmail.com>
2021-02-25 11:37:30 -0300

    perl template (#346)
    * perl template
    
    Signed-off-by: JoaoDanielRufino <joaodaniel0405@gmail.com>
    
    * pre-commit fix
    
    Signed-off-by: JoaoDanielRufino <joaodaniel0405@gmail.com>
    
    * fixed windows build
    
    Signed-off-by: JoaoDanielRufino <joaodaniel0405@gmail.com>

---
+++ templates/create_formula/languages/perl/build.bat
@@ -0,0 +1,31 @@
+:: Perl parameters
+echo off
+SETLOCAL
+SET BIN_FOLDER=bin
+SET BAT_FILE=%BIN_FOLDER%\run.bat
+SET SH_FILE=%BIN_FOLDER%\run.sh
+:build
+    mkdir %BIN_FOLDER%
+    xcopy /E /I src %BIN_FOLDER%
+    cd %BIN_FOLDER%
+    cd ..
+    call :BAT_WINDOWS
+    call :SH_LINUX
+    call :CP_DOCKER
+    GOTO DONE
+
+:BAT_WINDOWS
+    echo @ECHO OFF > %BAT_FILE%
+    echo SET mypath=%%~dp0 >> %BAT_FILE%
+    echo start /B /D "%%mypath%%" /WAIT perl -I ./ main.pl >> %BAT_FILE%
+
+:SH_LINUX
+    echo perl -I "$(dirname "$0")" "$(dirname "$0")"/main.pl > %SH_FILE%
+    GOTO DONE
+
+:CP_DOCKER
+    copy Dockerfile %BIN_FOLDER%
+    copy set_umask.sh %BIN_FOLDER%
+    GOTO DONE
+
+:DONE
---
+++ templates/create_formula/languages/perl/build.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+BINARY_NAME_UNIX=run.sh
+BINARY_NAME_WINDOWS=run.bat
+BIN_FOLDER=bin
+
+
+# Perl-Build:
+	mkdir -p $BIN_FOLDER
+	cp -r src/* "$BIN_FOLDER"
+
+	#Unix
+	{
+	echo "#!/bin/sh"
+	echo "perl -I \$(dirname \"\$0\") \$(dirname \"\$0\")/main.pl"
+	} >>  $BIN_FOLDER/$BINARY_NAME_UNIX
+	chmod +x "$BIN_FOLDER/$BINARY_NAME_UNIX"
+
+	#Windows
+	{
+  echo "@ECHO OFF"
+  echo "SET mypath=%%~dp0"
+  echo "start /B /D %%mypath%% /WAIT perl -I ./ main.pl"
+  } >> $BIN_FOLDER/$BINARY_NAME_WINDOWS
+
+#Docker Files:
+	cp Dockerfile set_umask.sh $BIN_FOLDER
