ac4963dfd9ff7f69612432e472d8b40f7eae0775
JoaoDanielRufino <joaodaniel0405@gmail.com>
2021-03-01 16:14:49 -0300

    node and typescript dependencies checked
    Signed-off-by: JoaoDanielRufino <joaodaniel0405@gmail.com>

--- templates/create_formula/languages/node/build.sh
+++ templates/create_formula/languages/node/build.sh
@@ -4,8 +4,17 @@ BINARY_NAME_UNIX=run.sh
 BINARY_NAME_WINDOWS=run.bat
 BIN_FOLDER=bin
 
+checkCommand () {
+  if ! command -v "$1" >/dev/null; then
+    echo "$1 required" >&2;
+		exit 1;    		
+	fi
+}
+
 
 # Node-Build:
+	checkCommand npm
+	checkCommand node
 	mkdir -p $BIN_FOLDER
 	cp -r src/* "$BIN_FOLDER"
 	npm install --silent --no-progress --prefix "$BIN_FOLDER"
