56f7f7c86beed0ccc1bd6b947b1a2836a38e611d
João Daniel Rufino <joaodaniel0405@gmail.com>
2021-03-09 10:10:43 -0300

    Build dependecies check  (#352)
    * node and typescript dependencies checked
    
    
    * pre-commit
    
    
    * perl dependencies
    
    
    * csharp dependencies
    
    
    * rust dependencies
    
    
    * change rust to cargo
    
    
    * python2 and python3 build dependencies
    
    
    * pre-commit

--- templates/create_formula/languages/csharp/build.sh
+++ templates/create_formula/languages/csharp/build.sh
@@ -7,7 +7,15 @@ BIN_FOLDER_DARWIN=darwin
 BIN_UNIX=src.dll
 SH=$BIN_FOLDER/run.sh
 
+checkCommand () {
+  if ! command -v "$1" >/dev/null; then
+    echo "$1 required" >&2;
+		exit 1;
+	fi
+}
+
 #linux-build:
+	checkCommand dotnet
 	mkdir -p $BIN_FOLDER
 	cp -r src/* $BIN_FOLDER
 	dotnet build $BIN_FOLDER/$SOURCE_FILE -o $BIN_FOLDER/$BIN_FOLDER_LINUX --configuration Release
