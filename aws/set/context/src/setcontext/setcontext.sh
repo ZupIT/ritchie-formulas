cc3f610ecea9c55cdb2562bb081ed3ae2fad57c6
maurineimirandazup <maurinei.miranda@zup.com.br>
2021-04-20 16:39:53 -0300

    First version
    Signed-off-by: maurineimirandazup <maurinei.miranda@zup.com.br>

--- aws/set/context/src/setcontext/setcontext.sh
+++ aws/set/context/src/setcontext/setcontext.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 run() {
-	< ~/.aws/credentials tr ' ' _ | nl
-	head -n -3 > /tmp/temp_cred
-	echo '[default]' >> /tmp/temp_cred
-	< ~/.aws/credentials tr ' ' _ | nl
-	grep "$CONTEXT_NAME" -A 2 | tail -n 2 >> /tmp/temp_cred
+	# replace de default context
+	cat ~/.aws/credentials > /tmp/temp_cred
+	echo -e "\n __________________________________"
+	echo -e "\n ~/.aws/credentials BEFORE CHANGE: \n"
+	cat ~/.aws/credentials
+	sed -i "s/default/backupDefault/" /tmp/temp_cred
+	sed -i "s/$CONTEXT_NAME/default/" /tmp/temp_cred
+	sed -i "s/backupDefault/$CONTEXT_NAME/" /tmp/temp_cred
 	cat /tmp/temp_cred > ~/.aws/credentials
+	echo -e "\n __________________________________"
+	echo -e "\n ~/.aws/credentials AFTER CHANGE: \n"
+	cat ~/.aws/credentials
 }
-
-run
