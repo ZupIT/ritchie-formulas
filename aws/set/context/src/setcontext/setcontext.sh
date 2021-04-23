7f6dca9f35212cdd700a1441a5b46d56a4701f78
Maurinei Cabral <67597621+maurineimirandazup@users.noreply.github.com>
2021-04-23 10:56:32 -0300

    New version of rit aws set context (#365)
    * First version
    
    Signed-off-by: maurineimirandazup <maurinei.miranda@zup.com.br>
    
    * LinterFix
    
    Signed-off-by: maurineimirandazup <maurinei.miranda@zup.com.br>
    
    * Check if context exist
    
    Signed-off-by: maurineimirandazup <maurinei.miranda@zup.com.br>
    
    * Update readme and add gif
    
    Signed-off-by: maurineimirandazup <maurinei.miranda@zup.com.br>
    
    * Add gif from direct link
    
    * lint markdown
    
    Signed-off-by: maurineimirandazup <maurinei.miranda@zup.com.br>

--- aws/set/context/src/setcontext/setcontext.sh
+++ aws/set/context/src/setcontext/setcontext.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 run() {
-	< ~/.aws/credentials tr ' ' _ | nl
-	head -n -3 > /tmp/temp_cred
-	echo '[default]' >> /tmp/temp_cred
-	< ~/.aws/credentials tr ' ' _ | nl
-	grep "$CONTEXT_NAME" -A 2 | tail -n 2 >> /tmp/temp_cred
-	cat /tmp/temp_cred > ~/.aws/credentials
-}
+	# check if context exist
+	exist=$(grep "$CONTEXT_NAME" < ~/.aws/credentials)
+	if [ "[$CONTEXT_NAME]" != "$exist" ]; then
+	echo "'$CONTEXT_NAME' context does not exist."
+	echo "Run 'cat ~/.aws/credentials' to see the avaible contexts." >&2
+	exit
+	fi
 
-run
+	# replace de default context
+	cat ~/.aws/credentials > /tmp/temp_cred
+	echo -e "______________~/.aws/credentials BEFORE change:_________________\
+	"
+	cut -b -30 ~/.aws/credentials
+	sed -i "s/default/backupDefault/" /tmp/temp_cred
+	sed -i "s/$CONTEXT_NAME/default/" /tmp/temp_cred
+	sed -i "s/backupDefault/$CONTEXT_NAME/" /tmp/temp_cred
+	cat /tmp/temp_cred > ~/.aws/credentials
+	echo -e "______________~/.aws/credentials AFTER change:__________________\
+	"
+	cut -b -30 ~/.aws/credentials
+}
